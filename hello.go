9e11b99a6acf0ab928bba6023c3072a3b0ed6f12
lunarmaniac <egoism@riseup.net>
2025-09-07 19:55:54 +0100

    refactor: improve tips loading mechanism and enhance API response structure

--- hello.go
+++ hello.go
@@ -3,41 +3,75 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/fs"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
-	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
-type Tips struct {
-	Tips []string `json:"tips"`
+type Tip struct {
+	Tip      string `json:"tip"`
+	Category string `json:"category"`
+}
+
+var tipsByCategory = make(map[string][]Tip)
+var allTips []Tip
+
+// load all JSON files from the folder
+func loadTips(folder string) error {
+	return filepath.WalkDir(folder, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() || !strings.HasSuffix(d.Name(), ".json") {
+			return nil
+		}
+
+		fileBytes, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		var tips []Tip
+		if err := json.Unmarshal(fileBytes, &tips); err != nil {
+			return err
+		}
+
+		for _, tip := range tips {
+			tipsByCategory[tip.Category] = append(tipsByCategory[tip.Category], tip)
+			allTips = append(allTips, tip)
+		}
+
+		return nil
+	})
+}
+
+func randomTipFromSlice(tips []Tip) Tip {
+	rand.Seed(time.Now().UnixNano())
+	return tips[rand.Intn(len(tips))]
 }
 
 func main() {
-	jsonFile, err := os.Open("tips.json")
-	if err != nil {
-		fmt.Println(err)
+	if err := loadTips("./tips"); err != nil {
+		log.Fatalf("Failed to load tips: %v", err)
 	}
 
-	byteValue, _ := io.ReadAll(jsonFile)
-	var tips Tips
-	json.Unmarshal(byteValue, &tips)
-
-	fmt.Println("Starting API server")
-
-	s := rand.NewSource(time.Now().Unix())
-	R := rand.New(s)
-
 	http.HandleFunc("/tip", func(w http.ResponseWriter, r *http.Request) {
-		i := R.Intn(len(tips.Tips))
-		randomTip := tips.Tips[i]
-
+		if len(allTips) == 0 {
+			http.Error(w, "No tips available", http.StatusInternalServerError)
+			return
+		}
+		tip := randomTipFromSlice(allTips)
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"tip": randomTip})
+		encoder := json.NewEncoder(w)
+		encoder.SetEscapeHTML(false)
+		encoder.Encode(map[string]string{"tip": tip.Tip, "category": tip.Category})
 	})
-
+	
+	fmt.Println("Starting API server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
